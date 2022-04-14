9fe7f5350b8a19f47fa91db4bbd497c63ee49f0d
PerhapYs <370060080@qq.com>
2022-04-14 22:28:49 +0800

    test 1.0.0.3

--- PerhapYs/Classes/JXPagingListContainerView+JXSegmentedViewListContainer.swift
+++ PerhapYs/Classes/JXPagingListContainerView+JXSegmentedViewListContainer.swift
@@ -6,9 +6,11 @@
 //
 
 import Foundation
+#if canImport(JXPagingView) && canImport(JXSegmentedView)
 import JXPagingView
 import JXSegmentedView
 
 extension JXPagingListContainerView: JXSegmentedViewListContainer {
     
 }  //  用于 将pagerView的listContainerView联动到 jxSegmentedView的listContainer
+#endif
